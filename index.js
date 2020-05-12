bcc7360e177a8b3b83e40e90dcab0b8779adca89
Josh Luerssen <joshluer@gmail.com>
2020-05-12 16:57:56 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,30 @@
+var lowercase = "hello!";
+
+var uppercase = "HELLO!";
+
+var mixedCase = "Hi there!";
+
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(string) {
+  if (lowercase.toLowerCase() === lowercase) {
+    return "I can't hear you!";
+  }
+  else if (uppercase.toUpperCase() === uppercase) {
+    return "YES INDEED!";
+  }
+}
