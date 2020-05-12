98410e2b040cde7332efc98c0376d646b10686fe
Josh Luerssen <joshluer@gmail.com>
2020-05-12 18:10:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function sayHiToGrandma(string) {
   if (uppercase.toUpperCase() === uppercase) {
     return "YES INDEED!";
   }
-  if (string === "I love you, Grandma.") {
+  if (mixedCase === "I love you, Grandma.") {
     return "I love you, too.";
   }
 }
